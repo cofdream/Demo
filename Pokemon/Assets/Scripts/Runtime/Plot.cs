3e17bb4d9fac55517b7d2eab567b2334ca524d97
cofdream <cofdream@sina.com>
2021-11-08 23:17:09 +0800

    update

---
+++ Pokemon/Assets/Scripts/Runtime/Plot.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pekemon
+{
+    public class Plot : ScriptableObject
+    {
+        [SerializeField, TextArea] private string description;
+        [TextArea(3, 6)] public string[] content;
+        public string command;
+        public Plot next;
+    }
+}
