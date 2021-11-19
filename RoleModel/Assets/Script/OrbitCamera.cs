8d2f9ed79d10b1cddbe61d3940b68a2d1f8bd159
cofdream <cofdream@sina.com>
2021-11-19 17:55:11 +0800

    Init

---
+++ RoleModel/Assets/Script/OrbitCamera.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitCamera : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform focus;
+
+    [SerializeField, Min(0f)]
+    float focusRadius = 1f;
+
+    [SerializeField, Range(1f, 20f)]
+    float distance = 5f;
+
+
+    [SerializeField, Range(0, 1f)]
+    float focusCentering = 0.5f;
+
+    [SerializeField, Range(1f, 360f)]
+    float rotationSpeed = 90f;
+
+    [SerializeField, Range(-89f, 89f)]
+    float minVerticalAngle = -30f, maxVerticalAngel = 60f;
+
+    [SerializeField, Min(0f)]
+    float alignDelay = 5f;
+
+    Vector3 focusPoint, previousFocusPoint;
+
+    Vector2 orbitAngles = new Vector2(45f, 0);
+
+    float lastManualRotationTime;
+
+    private void Awake()
+    {
+        focusPoint = focus.position;
+
+        transform.localRotation = Quaternion.Euler(orbitAngles);
+    }
+
+    private void LateUpdate()
+    {
+        UpdateFocusePoint();
+
+        Quaternion lookRotation;
+        if (ManualRotation() || AutomaticRotation())
+        {
+            ConstrainAngles();
+            lookRotation = Quaternion.Euler(orbitAngles);
+        }
+        else
+        {
+            lookRotation = transform.localRotation;
+        }
+
+        Vector3 lookDirection = lookRotation * transform.forward;
+
+        Vector3 lookPosition = focusPoint - lookDirection * distance;
+
+        transform.SetPositionAndRotation(lookPosition, lookRotation);
+    }
+
+    private void OnValidate()
+    {
+        if (maxVerticalAngel < minVerticalAngle)
+        {
+            maxVerticalAngel = minVerticalAngle;
+        }
+    }
+
+    private void UpdateFocusePoint()
+    {
+        previousFocusPoint = focusPoint;
+
+        Vector3 targetPoint = focus.position;
+
+        if (focusRadius > 0f)
+        {
+            float distance = Vector3.Distance(targetPoint, focusPoint);//获取当前聚焦点 两次移动的 间隔距离
+
+            float t = 1f;
+            if (distance > 0.01f && focusCentering > 0f)
+            {
+                t = Mathf.Pow(1f - focusCentering, Time.unscaledDeltaTime);
+            }
+
+
+            if (distance > focusRadius)
+            {
+                t = Mathf.Min(t, focusRadius / distance);
+            }
+
+            focusPoint = Vector3.Lerp(targetPoint, focusPoint, t);
+        }
+        else
+            focusPoint = targetPoint;
+    }
+    bool ManualRotation()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("CameraVertical"), Input.GetAxis("CameraHorizontal"));
+
+        const float e = 0.001f;
+
+        if (input.x < -e || input.x > e || input.y < -e || input.y > e)
+        {
+            orbitAngles += rotationSpeed * Time.unscaledDeltaTime * input;
+
+            lastManualRotationTime = Time.unscaledDeltaTime;
+
+            return true;
+        }
+        return false;
+    }
+
+    void ConstrainAngles()
+    {
+        orbitAngles.x = Mathf.Clamp(orbitAngles.x, minVerticalAngle, maxVerticalAngel);
+
+        if (orbitAngles.y < 0f)
+        {
+            orbitAngles.y += 360f;
+        }
+        else if (orbitAngles.y > 360f)
+        {
+            orbitAngles.y -= 360f;
+        }
+    }
+
+    bool AutomaticRotation()
+    {
+        if (Time.unscaledDeltaTime - lastManualRotationTime < alignDelay)
+        {
+            return false;
+        }
+
+        Vector2 movement = new Vector2(focusPoint.x - previousFocusPoint.x, focusPoint.z - previousFocusPoint.z);
+
+        float movementDeltaSqr = movement.sqrMagnitude;
+        if (movementDeltaSqr < 0.000001f)
+        {
+            return false;
+        }
+
+        float headingAngle = GetAngle(movement / Mathf.Sqrt(movementDeltaSqr));
+        float rotationChange = rotationSpeed * Time.unscaledDeltaTime;
+        orbitAngles.y = Mathf.MoveTowardsAngle(orbitAngles.y, headingAngle, rotationChange);
+
+        return true;
+    }
+
+    static float GetAngle(Vector2 direction)
+    {
+        float angle = Mathf.Acos(direction.y) * Mathf.Rad2Deg;
+        return direction.x < 0 ? 360f - angle : angle;
+    }
+}
---
+++ RoleModel/Assets/Script/PlayerControler.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    [Range(0, 100f)]
+    public float maxSpeed = 10f;
+
+    [Header("加速度")]
+    [Range(0, 100f)]
+    public float maxAcceleration = 10f;
+
+    [SerializeField]
+    Rect allowedArea = new Rect(-4.5f, -4.5f, 9, 9);
+
+    [SerializeField, Range(0f, 1f)]
+    float bounchiness = 0.5f;
+
+    Vector3 velocity;
+
+    public Vector2 playerInput;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+
+        Vector3 desiredVelocity = new Vector3(playerInput.x, 0, playerInput.y) * maxSpeed;//期望速度
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+
+        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+        velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+
+        Vector3 displacement = velocity * Time.deltaTime;
+
+        Vector3 newPosition = transform.localPosition + displacement;
+
+        //if (newPosition.x < allowedArea.xMin)
+        //{
+        //    newPosition.x = allowedArea.xMin;
+        //    velocity.x = -velocity.x * bounchiness;
+        //}
+        //else if (newPosition.x > allowedArea.xMax)
+        //{
+        //    newPosition.x = allowedArea.xMax;
+        //    velocity.x = -velocity.x * bounchiness;
+        //}
+
+        //if (newPosition.z < allowedArea.yMin)
+        //{
+        //    newPosition.z = allowedArea.yMin;
+        //    velocity.z = -velocity.z * bounchiness;
+        //}
+        //else if (newPosition.z > allowedArea.yMax)
+        //{
+        //    newPosition.z = allowedArea.yMax;
+        //    velocity.z = -velocity.z * bounchiness;
+        //}
+
+        transform.localPosition = newPosition;
+    }
+}
