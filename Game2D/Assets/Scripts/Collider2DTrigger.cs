1efc5e039f2091432c1c328762f20a4f2cc3f179
cofdream <cofdream@sina.com>
2022-01-14 18:06:36 +0800

    add 2d game

---
+++ Game2D/Assets/Scripts/Collider2DTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collider2DTrigger : MonoBehaviour
+{
+    public LayerMask layerMask;
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.IsTouchingLayers)
+        {
+
+        }    
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
---
+++ Game2D/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,38 @@
+﻿using Cofdream.ToolKit;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    [SerializeField] new Rigidbody2D rigidbody2D;
+
+    [SerializeField, ReadOnly] float maxSpeedX;
+
+    public float jump = 3f;
+    public bool canJump;
+
+    public Collider2DTrigger trigger;
+
+    void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (canJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            //rigidbody2D.velocity = new Vector2(Mathf.Max(rigidbody2D.velocity.x, maxSpeedX), rigidbody2D.velocity.y);
+        }
+        float x = Input.GetAxis("Horizontal");
+
+        if (x != 0)
+        {
+            rigidbody2D.AddForce(new Vector2(x, 0));
+        }
+
+
+        rigidbody2D.velocity = new Vector2(Mathf.Max(rigidbody2D.velocity.x, maxSpeedX), rigidbody2D.velocity.y);
+    }
+}
